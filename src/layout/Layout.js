6e5e7ec6db7421d7b47c7bac32a4f8b95de5599a
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-01 20:10:06 -0300

    Definido layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.forceMathUpdate = this.forceMathUpdate.bind(this);
+    }
+    
+    forceMathUpdate() {
+        this.forceUpdate();
+    }
+
     render() {
         return (
-            <p>Hola, mundo!</p>
+            <div>
+                <button className="btn btn-primary" onClick={this.forceMathUpdate}>
+                    Force Update
+                </button>
+                <hr />
+                <p>Math.random(): {Math.random()}</p>
+            </div>
+            
         );
     }
 }
